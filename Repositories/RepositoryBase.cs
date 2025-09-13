74bc1ac1d401270272753857d83cdd7b6aec6eaf
kermtuncr19 <kerimtuncer190748@gmail.com>
2025-09-13 18:45:54 +0300

    IRepositoryBase and IProductRepository were added.

---
+++ Repositories/RepositoryBase.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+
+namespace Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T>
+    where T : class, new()
+    {
+        protected readonly RepositoryContext _context;
+//devraldığımız class larda contexte ihtiyacımız olursa kullanabilelim diye protected yaptık private yerine.
+        protected RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> FindAll(bool trackChange)
+        {
+            return trackChange
+                ? _context.Set<T>()
+                : _context.Set<T>().AsNoTracking();
+        }
+    }
+
+    
+}
--- Repositories/obj/Debug/net9.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net9.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bdda52ab99c82655beaff53f64ea9d731dc81a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff287bb350072e05104daf9af4dc8b84d0d44f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
